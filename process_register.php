091426f14eeae1d5bdeebc57b1637c9096a8ab3f
aKanneh2003 <kannehahmed00@gmail.com>
2024-05-21 11:37:57 +0200

    Add files via upload

---
+++ process_register.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Change this to your database username
+$password = ""; // Change this to your database password
+$dbname = "user_management";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Encrypt the password
+
+    $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        header('Location:login.php');
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+
+
+?>
