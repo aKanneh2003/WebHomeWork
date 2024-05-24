b14fb7af3553aa39f8b4ea3ff8873273f503038e
aKanneh2003 <kannehahmed00@gmail.com>
2024-05-24 13:15:17 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'site_name' => 'Travel',
+    'menu' => [
+        'home' => 'Home',
+        'about' => 'About Us',
+        'package' => 'Packages',
+        'book' => 'Book',
+        'login' => 'Login'
+    ],
+];
