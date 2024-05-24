bc2a6699b3d70e871ca0d65866fe1f4d1fce73f5
aKanneh2003 <kannehahmed00@gmail.com>
2024-05-24 15:46:01 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,37 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title><?php echo ucfirst($page); ?></title>
-
-   <!-- swiper css link  -->
-   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
-   
-<!-- header section starts  -->
-
 <section class="header">
-
    <a href="index.php" class="logo">travel.</a>
-
    <nav class="navbar">
-      <?php foreach ($menu as $key => $value): ?>
-         <a href="index.php?page=<?php echo $key; ?>"><?php echo $value; ?></a>
-      <?php endforeach; ?>
+      <a href="index.php?page=home">home</a>
+      <a href="index.php?page=about">about</a>
+      <a href="index.php?page=package">packages</a>
+      <a href="index.php?page=book">book</a>
+      <a href="index.php?page=gallery">gallery</a>
+      <a href="index.php?page=login">login</a>
    </nav>
-
    <div id="menu-btn" class="fas fa-bars"></div>
-
 </section>
-
-<!-- header section ends -->
