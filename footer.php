d2d97031dc30fd28bdec74f53b19125ad5388552
aKanneh2003 <kannehahmed00@gmail.com>
2024-06-02 14:47:00 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,52 +1,6 @@
-<!-- footer section starts  -->
-
-<section class="footer">
-
-   <div class="box-container">
-
-      <div class="box">
-         <h3>quick links</h3>
-         <?php foreach ($menu as $key => $value): ?>
-            <a href="index.php?page=<?php echo $key; ?>"> <i class="fas fa-angle-right"></i> <?php echo $value; ?></a>
-         <?php endforeach; ?>
-      </div>
-
-      <div class="box">
-         <h3>extra links</h3>
-         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
-      </div>
-
-      <div class="box">
-         <h3>contact info</h3>
-         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
-         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
-         <a href="#"> <i class="fas fa-envelope"></i> Alfred-Ahmed@gmail.com </a>
-         <a href="#"> <i class="fas fa-map"></i> Kecskemet, Hungary 6000 </a>
-      </div>
-
-      <div class="box">
-         <h3>follow us</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-      </div>
-
-   </div>
-
-   <div class="credit"> created by <span>mr. Alfred  web designer</span> | all rights reserved! </div>
-
-</section>
-<!-- footer section ends -->
-
-<!-- swiper js link  -->
-<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
-
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-
+</main>
+    <footer class="text-center mt-5">
+        <p>&copy; 2024 Visit Salone</p>
+    </footer>
 </body>
 </html>
