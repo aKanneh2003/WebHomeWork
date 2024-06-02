4d12cee481fce285b26cae60eeff6a5e23010f52
aKanneh2003 <kannehahmed00@gmail.com>
2024-06-02 15:11:22 +0200

    Update about.php

--- about.php
+++ about.php
@@ -1,13 +1,6 @@
-<section class="home-about">
-
-   <div class="image">
-      <img src="images/about-img.jpg" alt="">
-   </div>
-
-   <div class="content">
-      <h3>about us</h3>
-      <p>Discover hidden sites with us!</p>
-      <a href="index.php?page=about" class="btn">read more</a>
-   </div>
-
-</section>
+<h1>About Visit Salone</h1>
+<p>Welcome to Visit Salone, your number one source for all things travel in Sierra Leone. We're dedicated to giving you the very best of travel experiences, with a focus on adventure, culture, and relaxation.</p>
+<p>Founded in 2023, Visit Salone has come a long way from its beginnings. When we first started out, our passion for showcasing Sierra Leone's beauty drove us to do intense research, and gave us the impetus to turn hard work and inspiration into a booming online travel guide.</p>
+<p>We hope you enjoy our travel recommendations as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</p>
+<p>Sincerely,</p>
+<p>The Visit Salone Team</p>
